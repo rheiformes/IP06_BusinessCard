79a3e4c106c5a53e515f1fcca778b08df490381c
rheiformes <rhearai77@gmail.com>
2022-10-18 11:09:45 -0500

    Initial Commit

---
+++ IP06_BusinessCard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IP06_BusinessCard
+//
+//  Created by Rai, Rhea on 10/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
